b9f062d7862dfb01a3386e217b37dd43cf0759ba
comfuture <comfuture@iplateia.com>
2021-07-06 13:03:29 +0000

    implement tests

--- test/conftest.py
+++ test/conftest.py
@@ -5,17 +5,13 @@ import pytest
 project_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_path)
 
-from server import create_app
+from fastapi.testclient import TestClient
+from server import app
 
 @pytest.fixture
-def flask_app():
-    config = {
-        'DEBUG': True
-    }
-    app = create_app(config=config)
+def fastapi_app():
     yield app
 
 @pytest.fixture
-def client(flask_app):
-    client = flask_app.test_client()
-    yield client
+def client(fastapi_app):
+    yield TestClient(fastapi_app)
--- test/test_fastapi.py
+++ test/test_fastapi.py
@@ -1,14 +1,15 @@
 import pytest
 
+
 def test_hello_api(client):
     # invoke without query
-    rv = client.get('/hello', content_type='application/json')
+    rv = client.get('/hello')
     assert rv.status_code == 200
-    data = rv.get_json()
+    data = rv.json()
     assert data.get('greet') == 'Hello, World!'
 
     # invoke with query `name`
-    rv = client.get('/hello?name=comfuture', content_type='application/json')
+    rv = client.get('/hello?name=comfuture')
     assert rv.status_code == 200
-    data = rv.get_json()
+    data = rv.json()
     assert data.get('greet') == 'Hello, comfuture!'
